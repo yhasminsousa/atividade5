6dbce8e1694a246d589f7bbe1d9e1b298ceaa4e0
palomalindinha01 <palomalindinha35@gmail.com>
2023-10-16 15:46:31 -0300

    first commit

---
+++ atividade05.py
@@ -0,0 +1,8 @@
+# Digite uma metragem e veja isso em cm e mm:
+met1 = float(input("quantidade em metros:"))
+cm = (met1*100)
+print("a quantidade em centimetros é:")
+print (cm)
+mm = (met1*1000)
+print("a quantidade em milimetros é:")
+print (mm)
